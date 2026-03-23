2469ecf823aa84f7879b65ac4daf7ff9fb82c552
Elio Grieco <grieco@egx.com>
2026-03-22 17:11:10 -0700

    refactor(cli): Refactor CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,10 @@
-use clap::{Parser, Subcommand};
+pub mod options;
+
+use clap::Parser;
 use symplasma::sources::Source;
 use symplasma::{find, find_or_create, list_items, list_sources};
 
-#[derive(Parser)]
-#[command(name = "symplasma")]
-#[command(about = "A CLI app to handle entities and common functionality for the Symplasma project")]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// List sources or items from a source
-    List {
-        #[command(subcommand)]
-        what: ListCommands,
-    },
-    /// Find the path to a file
-    Find {
-        /// Optional source type to scope the search
-        #[arg(short, long)]
-        source: Option<Source>,
-        /// The file name to find
-        file_name: String,
-    },
-    /// Find the path to a file, or create it if it doesn't exist
-    FindOrCreate {
-        /// Optional source type to scope the search
-        #[arg(short, long)]
-        source: Option<Source>,
-        /// The file name to find or create
-        file_name: String,
-    },
-}
-
-#[derive(Subcommand)]
-enum ListCommands {
-    /// List all possible data sources
-    Sources,
-    /// List all items from a specific source
-    Items {
-        /// The source type to list items from
-        source: Source,
-    },
-}
+use crate::options::{Cli, Commands, ListCommands};
 
 fn main() {
     let cli = Cli::parse();
---
+++ src/options.rs
@@ -0,0 +1,48 @@
+use clap::{Parser, Subcommand};
+use symplasma::sources::Source;
+
+#[derive(Parser)]
+#[command(name = "symplasma")]
+#[command(
+    about = "A CLI app to handle entities and common functionality for the Symplasma project"
+)]
+pub(crate) struct Cli {
+    #[command(subcommand)]
+    pub(crate) command: Commands,
+}
+
+#[derive(Subcommand)]
+pub(crate) enum Commands {
+    /// List sources or items from a source
+    List {
+        #[command(subcommand)]
+        what: ListCommands,
+    },
+    /// Find the path to a file
+    Find {
+        /// Optional source type to scope the search
+        #[arg(short, long)]
+        source: Option<Source>,
+        /// The file name to find
+        file_name: String,
+    },
+    /// Find the path to a file, or create it if it doesn't exist
+    FindOrCreate {
+        /// Optional source type to scope the search
+        #[arg(short, long)]
+        source: Option<Source>,
+        /// The file name to find or create
+        file_name: String,
+    },
+}
+
+#[derive(Subcommand)]
+pub(crate) enum ListCommands {
+    /// List all possible data sources
+    Sources,
+    /// List all items from a specific source
+    Items {
+        /// The source type to list items from
+        source: Source,
+    },
+}
